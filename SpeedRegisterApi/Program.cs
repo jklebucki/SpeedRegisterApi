7c07dd1dc34870f25a684e2b06be59b6941664a2
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2022-07-09 16:56:49 +0200

    enable swagger in production

--- SpeedRegisterApi/Program.cs
+++ SpeedRegisterApi/Program.cs
@@ -21,11 +21,8 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 builder.WebHost.UseUrls("http://*:8080");
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("corsapp");
 app.UseAuthorization();
